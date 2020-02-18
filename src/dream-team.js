a557f220be3caee8dc69c7699c3c18351a6f06b2
Pavel Saikov <pavsaikov@gmail.com>
2020-02-18 18:45:12 +0300

    feat: make function 'createDreamTeam'

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,28 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if(!(members instanceof Array)) return false;
+
+  let name = '';
+
+  for(let i = 0; i < members.length; i++) {
+    if(typeof(members[i]) == 'string') {
+      let word = members[i];
+      word = word.trim();
+      // word = word.toUpperCase();
+      name += word[0].toUpperCase();
+    }
+  }
+
+  let arr = [];
+  for(let i = 0; i < name.length; i++) {
+    arr[i] = name[i];
+  }
+
+  arr.sort();
+
+  name = arr.toString();
+  while(name.includes(',')) { 
+    name = name.replace(',', '');
+  }
+
+  return name;
 };
