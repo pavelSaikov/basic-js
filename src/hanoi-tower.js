05757ca862640b2ff52cd63f2ceda7da50ac6f4a
Pavel Saikov <pavsaikov@gmail.com>
2020-02-19 18:47:04 +0300

    feat: make functions 'transform' and 'caclulateHanoi'

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,4 +1,18 @@
-module.exports = function calculateHanoi(/* disksNumber, turnsSpeed */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function calculateHanoi( disksNumber, turnsSpeed) {
+    let turnsNumber = 0;
+    for(let i = 1; i <= disksNumber; i++) {
+        if(i == 1) {
+            turnsNumber = 1;
+        } else {
+            turnsNumber = 2 * turnsNumber + 1;
+        }
+    }
+
+    let seconds = (turnsNumber / turnsSpeed) * 3600;
+
+    let result = {};
+    result['turns'] = turnsNumber;
+    result['seconds'] = seconds;
+
+    return result;
 }
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,52 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+    if(!(arr instanceof Array)) throw new Error();
+
+    for(let i = 0; i < arr.length; i++) {
+        if(typeof(arr[i]) == 'string') {
+            switch (arr[i]) {
+                case '--discard-next':
+                    if(i + 1 != arr.length)
+                        arr.splice(i + 1, 1);
+
+                    arr.splice(i, 1);
+                    i--;
+
+                    break;
+                
+                case '--discard-prev':
+                    if(i != 0) {
+                        arr.splice(i, 1);
+                        arr.splice(i - 1, 1);
+                        i -= 2;
+                    } else {
+                        arr.splice(i, 1);
+                        i--;
+                    }
+
+                    break;
+
+                case '--double-next':
+                    if(i + 1 != arr.length) {
+                        arr[i] = arr[i + 1];
+                        //i++;
+                    } else {
+                        arr.splice(i, 1);
+                    }
+
+                    break;
+
+                case '--double-prev':
+                    if(i != 0) {
+                        arr[i] = arr[i - 1];
+                    } else {
+                        arr.splice(i, 1);
+                        i--;
+                    }
+
+                    break;
+            }
+        }
+    }
+
+    return arr;
 };
