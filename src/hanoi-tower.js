576e4ba4b7feef4a258c47d1f5554b52f3b9cc3a
Pavel-Laptop <pavsaikov@gmail.com>
2020-02-24 18:42:12 +0300

    fix: modify time calculation algorith

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,14 +1,14 @@
-module.exports = function calculateHanoi( disksNumber, turnsSpeed) {
+module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
     let turnsNumber = 0;
-    for(let i = 1; i <= disksNumber; i++) {
-        if(i == 1) {
+    for (let i = 1; i <= disksNumber; i++) {
+        if (i == 1) {
             turnsNumber = 1;
         } else {
             turnsNumber = 2 * turnsNumber + 1;
         }
     }
 
-    let seconds = (turnsNumber / turnsSpeed) * 3600;
+    let seconds = turnsNumber / (turnsSpeed / 3600);
 
     let result = {};
     result['turns'] = turnsNumber;
