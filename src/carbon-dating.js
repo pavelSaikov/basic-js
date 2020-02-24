2aeb3e49658daf7c3400f77def01aa4d12674ad8
Pavel-Laptop <pavsaikov@gmail.com>
2020-02-24 11:20:52 +0300

    fix: change base of logarithm in function

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,15 +3,20 @@ const HALF_LIFE_PERIOD= 5730;
 
 module.exports = function dateSample(sampleActivity) {
   if(!(typeof sampleActivity == "string")) return false;
+
+  if(sampleActivity.lastIndexOf('.') != sampleActivity.indexOf('.')) {
+    sampleActivity = sampleActivity.slice(0, sampleActivity.lastIndexOf('.'));  
+  }
+  
   if(isNaN(sampleActivity)) return false;
   if(new Number(sampleActivity) <= 0) return false;
   if(sampleActivity >= MODERN_ACTIVITY) return false;
   if(sampleActivity == null) return false;
 
   let ratio = MODERN_ACTIVITY / sampleActivity;
-  let degree = Math.log2(ratio);
-  let age = HALF_LIFE_PERIOD * degree;
-  age = Math.round(age);
+  let degree = Math.log(ratio);
+  let age = HALF_LIFE_PERIOD * degree / 0.693;
+  age = Math.ceil(age);
 
   return age;
 };
